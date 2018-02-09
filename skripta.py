642daefda57523976ba32f54e0c0ada06fbc040b
DjOka1914 <muticdjordje@gmail.com>
2018-02-09 12:27:12 +0100

    sitna izmena

--- skripta.py
+++ skripta.py
@@ -93,7 +93,7 @@ def addNumber(lista, broj, width, height, region, bbox):
             return False
     lista.append((broj, x, y, width))
 
-DIR = 'C:\Users\Mutic\Desktop\SoftProjekatDjole'
+DIR = 'C:\Users\Mutic\Desktop\SoftDjole\soft2017\Videos'
 mnistFile = 'mnistPrepared'
 
 mnist = fetch_mldata('MNIST original')
