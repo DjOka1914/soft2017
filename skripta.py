906a35b9a26839d0c4a8271896a7e45a561f672a
DjOka1914 <muticdjordje@gmail.com>
2018-02-10 16:19:26 +0100

    Prepoznavanje brojeva bolje

--- skripta.py
+++ skripta.py
@@ -1,9 +1,7 @@
 # u 96. redu obratiti paznju na putanju
 
-import os
 import os.path
 
-import matplotlib.pyplot as plt
 import numpy as np
 from skimage import img_as_ubyte
 from skimage.measure import label, regionprops
@@ -15,104 +13,120 @@ import cv2
 
 def prepareTrainData(data):
     close_kernel = np.ones((5, 5), np.uint8)
-    for i in range(0, len(data)):
+    len_data=len(data)
+    for i in range(0, len_data):
         number = data[i].reshape(28, 28)
         th = cv2.inRange(number, 150, 255)
         closing = cv2.morphologyEx(th, cv2.MORPH_CLOSE, close_kernel)
         labeled = label(closing)
         regions = regionprops(labeled)
-        if(len(regions) > 1):
-            max_width = 0
+        len_regions=len(regions)
+        if(len_regions > 1):
             max_height = 0
+            max_width = 0
             for region in regions:
                 t_bbox = region.bbox
-                t_width = t_bbox[3] - t_bbox[1]
                 t_height = t_bbox[2] - t_bbox[0]
-                if(max_width < t_width and max_height < t_height):
-                    max_height = t_height
-                    max_width = t_width
+                t_width = t_bbox[3] - t_bbox[1]
+                if(max_height < t_height and max_width < t_width):
                     bbox = t_bbox
+                    max_width = t_width
+                    max_height = t_height
         else:
             bbox = regions[0].bbox
-        img = np.zeros((28, 28))
         x = 0
-        for row in range(bbox[0], bbox[2]):
-            y = 0
-            for col in range(bbox[1], bbox[3]):
+        img = np.zeros((28, 28))
+        bbx1=bbox[1]
+        bbx2=bbox[2]
+        bbx0=bbox[0]
+        bbx3=bbox[3]
+        y = 0
+        for row in range(bbx0, bbx2):
+            for col in range(bbx1, bbx3):
                 img[x, y] = number[row, col]
-                y += 1
+                y_ink=y+1
+                y=y_ink
             x += 1
         data[i] = img.reshape(1, 784)
 
-def jednacinaPrave(x, y):
-    return x*(y2-y1) + y*(x1-x2) + (x2*y1-x1*y2)
-
 def getNumberImage(bbox, img):
-    min_row = bbox[0]
-    min_col = bbox[1]
-    width = bbox[3] - min_col
-    height = bbox[2] - min_row
+    bbx1 = bbox[1]
+    bbx2 = bbox[2]
+    bbx0 = bbox[0]
+    bbx3 = bbox[3]
+    height = bbx2 - bbox[0]  #bbox[0] je min_row
+    width = bbx3 - bbox[1]  #bbox[0] je min_col
     img_number = np.zeros((28, 28))
     for x in range(0, height):
         for y in range(0, width):
-            img_number[x, y] = img[min_row+x-1, min_col+y-1]
+            x_dekr=x-1
+            y_dekr=y-1
+            img_number[x, y] = img[x_dekr + bbox[0], y_dekr + bbox[1]]
     return img_number
 
 def presekSaPravom(bbox, height, width):
-    #if(x1 > bbox[3] and x2 > bbox[3]):#Linija je desno od pravouganika
-    #    return False
-    #if(x1 < bbox[1] and x2 < bbox[1]):#Linija je levo od pravouganika
-    #    return False
-    #if(y1 > bbox[0]+height/2 and y2 > bbox[0]+height/2):#Linija je iznad pravouganika
-    #    return False
-    #if(y1 < bbox[2] and y2 < bbox[2]):#Linija je ispod pravouganika
-    #    return False
-    if(bbox[2]+4 < y2 or bbox[3]+4 < x1 or bbox[1] > x2):
+    bbx3 = bbox[3]
+    bbx0 = bbox[0]
+    bbx1 = bbox[1]
+    bbx2 = bbox[2]
+    if( x2<(bbx1) or x1>(4+bbx3) or y2>(4+bbx2)):
         return False
-    recEnd = jednacinaPrave(bbox[3]+1, bbox[2]+1)
+    recEnd = (y1*x2-y2*x1)+ (x1-x2)*(1+bbx2) + (y2-y1)*(1+bbx3)
     if(recEnd <= 0): #Ukoliko je kraj regiona ispod linije racunati kao da je ceo pravouganik presao liniju
         return False
-    TL = jednacinaPrave(bbox[1], bbox[0])
-    TR = jednacinaPrave(bbox[3]+4, bbox[0])
-    BL = jednacinaPrave(bbox[1], bbox[2]+4)
-    BR = jednacinaPrave(bbox[3]+4, bbox[2]+4)
-    if(TL > 0 and TR > 0 and BL > 0 and BR > 0):
+    TL = (y1*x2-y2*x1)+ (x1-x2)*bbx0 + (y2-y1)*bbx1
+    TR = (y1*x2-y2*x1)+ (x1-x2)*bbx0 + (y2-y1)*(4+bbx3)
+    BL = (y1*x2-y2*x1)+ (x1-x2)*(4+bbx2) + (y2-y1)*bbx1
+    BR = (y1*x2-y2*x1)+ (x1-x2)*(4+bbx2) + (y2-y1)*(4+bbx3)
+    if (0 < BR and 0 < TR and 0 < TL and 0 < BL):
         return False
-    elif(TL < 0 and TR < 0 and BL < 0 and BR < 0):
+    elif (0 > BR and 0 > TR and 0 > TL and 0 > BL):
         return False
     else:
         return True
 
-def addNumber(lista, broj, width, height, region, bbox):
-    x=bbox[1]
-    y=bbox[0]
+def addNumber(width, lista, bbox, broj):
     for tup in lista:
-        if(tup[0] == broj and tup[1] < x+5  and tup[2] < y+5 and tup[3] == width):
+        t2=tup[2]
+        t1=tup[1]
+        t3=tup[3]
+        t0=tup[0]
+        if(width==t3 and (5+bbox[1])>t1 and broj==t0 and (5+bbox[0])>t2 ):
             lista.remove(tup)
-            lista.append((broj, x, y, width))
+            lista.append((broj, bbox[1], bbox[0], width))
             return False
-    lista.append((broj, x, y, width))
+    bbx0=bbox[0]
+    bbx1=bbox[1]
+    lista.append((broj, bbx1, bbx0, width))
 
-DIR = 'C:\Users\Mutic\Desktop\SoftDjole\soft2017'
-mnistFile = 'mnistPrepared'
+mnistOrString = 'MNIST original'
+mnistOriginal = fetch_mldata(mnistOrString)
 
-mnist = fetch_mldata('MNIST original')
-if(os.path.exists(os.path.join(DIR, mnistFile)+'.npy')):
-    train = np.load(os.path.join(DIR, mnistFile)+'.npy')
+DIRString = 'C:\Users\Mutic\Desktop\SoftProjekatDjole'
+DIR = DIRString
+if(os.path.exists(os.path.join(DIR, 'mnistPrepared')+'.npy')):
+    train = np.load(os.path.join(DIR, 'mnistPrepared')+'.npy')
 else:
-    train = mnist.data
+    train = mnistOriginal.data
+    np.save(os.path.join(DIR, 'mnistPrepared'), train)
     prepareTrainData(train)
-    np.save(os.path.join(DIR, mnistFile), train)
-train_labels = mnist.target
+train_labels = mnistOriginal.target
 knn = KNeighborsClassifier(n_neighbors=1, algorithm='brute').fit(train, train_labels)
 
 eros_kernel = np.ones((2, 2), np.uint8)#Kernel koriscen prilikom erozije (priprema za skeleton operaciju)
 close_kernel = np.ones((4, 4), np.uint8)
-DIR = DIR+'\\Videos'
-video_names = [os.path.join(DIR, name) for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]
-for vid_num in range(0, len(video_names)):
-    print 'Putanja do videa: ' + video_names[vid_num]
-    cap = cv2.VideoCapture(video_names[vid_num])
+
+video_imena = [os.path.join(DIR+'\\Videos', naziv) for naziv in os.listdir(DIR+'\\Videos') if os.path.isfile(os.path.join(DIR+'\\Videos', naziv))]
+#ODAVDE
+izlazniFajl = open("out.txt","w")
+izlazniFajl.write("Suma brojeva sa obradjenih video materijala: \n")
+izlazniFajl.write("RA 65/2013 Djordje Mutic\n")
+#DOVDE
+broj_snimaka=len(video_imena)
+for redniBrojSnimka in range(0, broj_snimaka):
+    videoPutanja=video_imena[redniBrojSnimka]
+    print 'Putanja do videa: ' + videoPutanja
+    cap = cv2.VideoCapture(videoPutanja)
     frameNum = 0
     lista_brojeva=[]
     while(cap.isOpened()):
@@ -147,7 +161,7 @@ for vid_num in range(0, len(video_names)):
             #cv2.line(gray,(x1,y1),(x2,y2),(255,255,255),1)
             #plt.imshow(gray, 'gray')
             #plt.show()
-            if(addNumber(lista_brojeva, num, width, height, region, bbox) == False):
+            if (addNumber(width, lista_brojeva, bbox, num) == False):
                 continue
             #print 'U frejmu '+ str(frameNum)+ '. prepoznat broj '+str(num)
         frameNum += 1
@@ -156,5 +170,11 @@ for vid_num in range(0, len(video_names)):
     for tup in lista_brojeva:
         suma += tup[0]
     print 'Suma: '+str(suma)+'\n'
+    #OVDE da upisem u out.txt resenje za video
+    videoSnimak = "videos/video-" + format(redniBrojSnimka) + ".avi"
+    nazivVideoSnimka = "video-" + format(redniBrojSnimka) + ".avi"
+    izlazniFajl.write("Naziv video snimka: " + str(videoSnimak) + " \n")
+    izlazniFajl.write("Rezultat: " + str(suma) + " \n")
+    #DOVDE
 cap.release()
 cv2.destroyAllWindows()
